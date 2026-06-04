939b64d2d3bd9b79db989b787e6daf475fb8623b
umayer10-AI <mdumayer10@gmail.com>
2026-06-04 19:07:17 +0600

    add

--- client/src/app/dashboard/company/page.jsx
+++ client/src/app/dashboard/company/page.jsx
@@ -1,8 +1,13 @@
 import { MyCompanies } from '@/component/dashboard/Company';
 import { HeaderSection } from '@/component/dashboard/CompanyHeader';
+import { getUserSession } from '@/lib/session';
 import React from 'react';
 
-const page = () => {
+const page = async () => {
+
+    const user = await getUserSession()
+    console.log(user)
+
     return (
         <div>
             <HeaderSection></HeaderSection>
