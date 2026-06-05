fcf83ae24ffb460a8b218d2507e5ce7bd5847cd8
umayer10-AI <mdumayer10@gmail.com>
2026-06-05 07:05:17 +0600

    add

--- client/src/component/JobsCard.jsx
+++ client/src/component/JobsCard.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import Jobs from './Jobs';
 
 export default async function JobDashboard() {
+  // Server-side database theke data fetch
   const jobsData = await getJobsData();
 
   return (
-    <div>
-      <Jobs jobsData={jobsData}></Jobs>
+    <div className="min-h-screen bg-gray-950 p-6 font-sans text-white">
+      {/* Client layout component e pure array data pass hobe */}
+      <Jobs initialJobs={jobsData} />
     </div>
   );
 }
